b642dcbd229af92c80cca335916e4c98e932650f
Melany Quispe <melany.ql.2006@gmail.com>
2026-09-09 17:38:23 -0500

    feat: se agrega componente OutlinedTextField

--- app/src/main/java/com/example/lab04_melany/MainActivity.kt
+++ app/src/main/java/com/example/lab04_melany/MainActivity.kt
@@ -3,9 +3,22 @@ package com.example.lab04_melany
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -28,6 +41,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContenidoLaboratorio() {
+    var textoEstado by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -53,6 +68,13 @@ fun ContenidoLaboratorio() {
                 )
             }
         }
+
+        // COMPONENTE 2: OutlinedTextField
+        OutlinedTextField(
+            value = textoEstado,
+            onValueChange = { textoEstado = it },
+            label = { Text("Ingresa tu nombre") },
+            modifier = Modifier.fillMaxWidth()
+        )
     }
-}
 
